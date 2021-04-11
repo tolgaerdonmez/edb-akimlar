70e9d329ef79c8bac820a06ec204a428e9de228b
Tolga Erdonmez <tolgaerdonmez@gmail.com>
2021-04-11 22:48:29 +0300

    avatar hover on changed

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -11,7 +11,9 @@ type Props = HTMLProps<HTMLImageElement> & IProps;
 export function Avatar({ src, title, alt, onClick, shape }: Props): ReactElement {
 	return (
 		<div className={`avatarContainer ${onClick ? "avatarSelectable" : ""}`} onClick={onClick}>
-			<img className={shape || "round"} src={src} alt={title || alt} />
+			<div className={shape || "round"}>
+				<img src={src} alt={title || alt} />
+			</div>
 			{title ? <p className="title">{title}</p> : null}
 		</div>
 	);
