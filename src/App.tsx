b3fbd4e8b6a0cfa57388036483e0b8028aad06fa
Tolga Erdonmez <tolgaerdonmez@gmail.com>
2021-04-20 14:16:01 +0300

    removed helmet

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,21 @@
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { AkimPage } from "./pages/AkimPage";
 import { Home } from "./pages/Home";
-import { Helmet } from "react-helmet";
 
 function App() {
   return (
-    <>
-      <Helmet>
-        <meta name="description" content="Edebiyat akımları ve sanatçıları" />
-        <meta property="og:title" content="Akımlar" />
-        <meta property="og:description" content={"Edebiyat akımları ve sanatçıları"} />
-        <meta property="og:type" content="website" />
-        <meta property="og:image" content="og.jpg" />
-      </Helmet>
-      <Router>
-        <div>
-          <Switch>
-            <Route path="/akimlar/:name?">
-              <AkimPage />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    </>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/akimlar/:name?">
+            <AkimPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
